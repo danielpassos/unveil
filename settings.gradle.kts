221941369f87545a79963065184fa8e08050ed5d
Daniel Passos <daniel@passos.me>
2026-03-26 06:43:22 -0300

    build: update settings.gradle to use sampleApp:composeApp module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,7 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "Unveil"
-include(":sampleApp")
+include(":sampleApp:composeApp")
 include(":unveil-core")
 include(":unveil-network")
 include(":unveil-network-ktor")
